bf9f98f07843978284c0abb6857e22922e0787a7
Ajay Verma <ajayvarma.brh572000@gmail.com>
2021-05-17 09:36:04 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Assignment from './Assignment'
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App"> 
+      <Assignment />
+
+
+    </div>   
+  );
+}
+
+export default App;
