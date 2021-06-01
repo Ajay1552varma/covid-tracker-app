8b09ccd8532247650302a2350e59002cde029741
Ajay Verma <ajayvarma.brh572000@gmail.com>
2021-06-01 16:54:42 +0530

    Revert "covid app to github"
    This reverts commit 73c7d8adc34bd5a7bb8c1306b9434d439f31c83a.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import logo from './logo.svg';
+import Assignment from './Assignment'
 import './App.css';
-import About from './About'
-import Covidapp from './Covidapp'
-import Practice from './Practice'
-import Home from './Home'
-import {Navbar,Nav,Form,Button,FormControl} from 'react-bootstrap'
-import {Link,Route,BrowserRouter as Router,Switch} from 'react-router-dom'
+
 function App() {
   
   return (
     <div className="App"> 
-    <Router>
- <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home"> <Link to="Home">Home</Link></Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Navbar.Brand href="#home"><Link to="Practice">Covid-19-District-Data</Link></Navbar.Brand>
-    <Navbar.Brand href="#home"><Link to="Covidapp">Covid-19-Pradesh-Data</Link></Navbar.Brand>
-    <Navbar.Brand href="#home"><Link to="About">About Us</Link></Navbar.Brand>
-      </Nav>
-      <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-      </Form>
-        </Navbar.Collapse>
-        </Navbar>
-    
-  <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/Covidapp">
-            <Covidapp />
-          </Route>
-          <Route path="/Practice">
-            <Practice />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-       </Router>
+      <Assignment />
+
+
     </div>   
   );
 }
